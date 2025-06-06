e0fcae54011b1c3da9292b9938cbfa469272f134
Iván Navatta <ivannavattaQgmail.com>
2025-06-06 09:03:53 -0300

    (feat): sprint 3

--- main.py
+++ main.py
@@ -30,17 +30,60 @@ def shows_employed():
     else:
         print("Empleados cargados:")
         for i in range(len(names)):
-            print(f"{i+1}. {names[i]} | Performance: {auto_performance[i]}, Potencial: {auto_potential[i]}")
+            print(f"{i+1}. {names[i]} | Auto-Performance: {auto_performance[i]}, Auto-Potencial: {auto_potential[i]}")
         print()
 
-# funcion main que sirve como "menu" para que ul usario elija que desea realizar.
+#funcion que sirve para que los empleados puedan actualizar sus datos
+def update_data():
+    employed = input("Ingrese su nombre y apellido: ")
+    while employed == "" :
+        employed = input("Ingrese su nombre y apellido: ")   
+    for i in range(len(names)):
+        employed_name = names[i]
+        if employed_name == employed:
+            print(f"name: {names[i]}")
+            print(f"Auto-Performance: {auto_performance[i]}")
+            print(f"Auto-Potencial: {auto_potential[i]}")
+            updated_info = int(input("Desea actualizar todos los campos? (Si, precione 1. No, precione -1): "))
+            while updated_info > 1 or updated_info < -1 :
+                updated_info = int(input("Desea actualizar todos los campos? (Si, precione 1. No, precione -1): "))
+            if updated_info == 1:
+                update_performance = int(input("Ingrese su nueva performance: "))
+                while update_performance < 1 or update_performance > 5:
+                    update_performance = int(input("Ingrese su nueva performance: "))
+                auto_performance[i] = update_performance
+                update_potential = int(input("Ingrese su nuevo potencial: "))
+                while update_potential < 1 or update_potential > 5:
+                    update_potential = int(input("Ingrese su nueva potencial: "))
+                auto_potential[i] = update_potential
+            elif updated_info == -1:
+                updated_info = int(input("Desea actualizar su performance o su potencial? (Performance, 1. Potencial, 2): "))
+                while updated_info > 2 or updated_info < 1:
+                    updated_info = int(input("Desea actualizar su performance o su potencial? (Performance, 1. Potencial, 2): "))
+                if updated_info == 1:
+                    update_performance = int(input("Ingrese su nueva performance: "))
+                    while update_performance < 1 or update_performance > 5 :
+                        update_performance = int(input("Ingrese su nueva performance: "))
+                    auto_performance[i] = update_performance
+                    return
+                elif updated_info == 2:
+                    update_potential = int(input("Ingrese su nuevo potencial: "))
+                while update_potential < 1 or update_potential > 5:
+                    update_potential = int(input("Ingrese su nuevo potencial: "))
+                auto_potential[i] = update_potential
+                
+            return
+    print("Empleado no encontrado")
+
+# funcion main que sirve como "menu" para que el usario elija que desea realizar.
 def main():
-    run = int(input("Desea correr el programa? (si, precione 1. no, precione -1) "))
+    run = int(input("Desea correr el programa? (si, precione 1. no, precione -1): "))
     while run != -1:
         if run == 1:
-            print("Opcion 1 cargar resultados")
-            print("Opcion 2 agregar empleado")
-            print("Opcion 3, salir")
+            print("Opcion 1, cargar resultados")
+            print("Opcion 2, agregar empleado")
+            print("Opcion 3, actualizar datos")
+            print("Opcion 4, salir")
 
             option = int(input("Ingrese una opcion: "))
             if option == 1:
@@ -48,13 +91,15 @@ def main():
             elif option == 2:
                 add_empoyed()
             elif option == 3:
+                update_data()
+            elif option == 4:
                 print("Fin del programa")
                 break
             else:
                 print("Opcion invalida")
         else:
             print("Opcion invalida")
-            run = int(input("Desea correr el programa? (si, precione 1. no, precione -1) "))
+            run = int(input("Desea correr el programa? (si, precione 1. no, precione -1): "))
 
 
 #ejecuta la funcion.
